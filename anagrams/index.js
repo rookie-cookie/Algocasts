e71734b9bd94e27db5c423752249aceed629f7e3
Jona Reyes <jonamreyes@gmail.com>
2018-06-21 19:27:42 -0400

    anagrams using char map technique

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  //Object.keys - returns the object's property NAME
+  //compare the number of objects in each charMap
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+    return false;
+  }
+
+  for (let char in aCharMap){
+    if (aCharMap[char] !== bCharMap[char]){
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
+//use a function helper to create character map
+function buildCharMap(str){
+  const charMap = {};
+  //remove any special characters by using regex and convert the string to lowercase
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    if (!charMap[char]){
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
+  }
+  return charMap;
+}
 
 module.exports = anagrams;
