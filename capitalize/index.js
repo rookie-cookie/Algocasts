13fa08d0d97073a1b4de5e8082ade15bf8304e00
Jona Reyes <jonamreyes@gmail.com>
2018-06-21 21:00:56 -0400

    capitalizing specific letters

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  //split the string by spaces
+  const words = str.split(" ");
+  const capitalized = [];
+  for (var i = 0; i < words.length; i++){
+    //capitalized the first letter then combine with the rest of the letters
+    //push it to the array
+    capitalized.push(words[i][0].toUpperCase() + words[i].slice(1));
+  }
+  //then join all words together with space in between
+  return capitalized.join(' ')
+}
 
 module.exports = capitalize;
