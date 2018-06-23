cc2ff120742f04e02bd5c0125b56d529e81853a8
Jona Reyes <jonamreyes@gmail.com>
2018-06-23 11:10:25 -0400

    printing a pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,25 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  //find the midpoint or middle index first
+  const midpoint = Math.floor((2 * n -1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    
+    //total column length for pyramid problem is n * 2 - 1
+    for (let col = 0; col < 2 * n -1; col++){
+      //need to add # to the left and right index of the midpoint
+      //ex. midpoint at 3rd index, at row = 1, 3-1=2 (2nd index) 3+1=4 (4th index)
+      if (midpoint - row <= col && midpoint + row >= col){
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
