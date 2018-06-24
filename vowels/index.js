117052d39039ae2abce746cab42062e02abe7568
Jona Reyes <jonamreyes@gmail.com>
2018-06-24 14:46:43 -0400

    finding vowels in a string

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //create an array that will hold all Vowels
+  const vowels = [];
+  //convert the string into an array
+  const string = str.toLowerCase().split('');
+
+  //find the vowels using regex
+  let check = string.join().match(/[aeiou]/g);
+  if (!check){
+    //if nothing is found, return 0
+    return 0;
+  } else {
+    for (var i = 0; i< check.length; i++){
+      vowels.push(check[i]);
+    }
+    //else return the length of the array
+    return vowels.length;
+  }
+}
 
 module.exports = vowels;
