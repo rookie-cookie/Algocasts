bc555c9e27eb86e72a9824172b6033bcfd928dd3
Jona Reyes <jonamreyes@gmail.com>
2018-06-28 19:47:40 -0400

    insertFirst

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -1,9 +1,36 @@
 // --- Directions
 // Implement classes Node and Linked Lists
-// See 'directions' document
+// See 'directions.html' document
 
-class Node {}
+//node implementation
+class Node {
+  //node - next node along the chain
+  constructor(data, next = null){
+    this.data = data;
+    this.next = next;
+  }
 
-class LinkedList {}
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+  insertFirst(data){
+    //remember the node class takes 2 arguments
+    //this.head becomes the second argument - it becomes the 'next' node
+    //and the 'data' node is inserted
+    this.head = new Node(data, this.head);
+  }
+}
+
+/*
+test:
+const nodeOne = new Node(5);
+const list = new LinkedList();
+list.head = nodeOne;
+list.insertFirst(15);
+list
+*/
 
 module.exports = { Node, LinkedList };
