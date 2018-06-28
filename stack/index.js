61807254d69906cb21048a11aefd3fc4ded4a0e9
Jona Reyes <jonamreyes@gmail.com>
2018-06-27 20:27:51 -0400

    stack implementation

--- stack/index.js
+++ stack/index.js
@@ -10,6 +10,24 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  //stack implementation
+  //remember - FILO (First in last out)
+  constructor(){
+    this.data = [];
+  }
+
+  push(record){
+    this.data.push(record);
+  }
+
+  pop(){
+    return this.data.pop();
+  }
+
+  peek(){
+   return this.data[this.data.length - 1]
+  }
+}
 
 module.exports = Stack;
