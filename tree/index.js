dc5422ff324732eec468e022487c573b65b1496b
Jona Reyes <jonamreyes@gmail.com>
2018-06-30 15:03:15 -0400

    tree traversal

--- tree/index.js
+++ tree/index.js
@@ -10,8 +10,57 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data){
+    //initialize data variable and children array
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data){
+    this.children.push(new Node(data));
+  }
+
+  remove(data){
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor(){
+    this.root = null;
+  }
+
+  //traverse by breadth first - start by iterating at each level of the Tree
+  traverseBF(fn){
+    //take the root node and push it in a new array
+    const arr = [this.root];
+    //then remove the elements in the array
+    while(arr.length){
+      const node = arr.shift();
+      //check if current element have children nodes
+      //add children nodes at the end of the array
+      //ES2015 Spread Operator - this will take every element in node.children
+      //and push that to the arr array
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  //traverse by depth first - start at the root and try to hit the bottom of the tree as quick as possible
+  traverseDF(fn){
+    //take the root node and push it in a new array
+    const arr = [this.root];
+    while(arr.length){
+      const node = arr.shift();
+      //unshift - add an element to the front of an array
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+
+}
 
 module.exports = { Tree, Node };
