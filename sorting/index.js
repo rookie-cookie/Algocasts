b66d9480a7ec80ab2c6300a1780659638e208846
Jona Reyes <jonamreyes@gmail.com>
2018-07-05 19:50:12 -0400

    bubble sort

--- sorting/index.js
+++ sorting/index.js
@@ -1,8 +1,20 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+//bubblesort and selectionsort - n^2 runtime
+//poor algorithm for large data sets
 
+function bubbleSort(arr) {
+  for (let i=0; i < arr.length; i++){
+    for (let j=0; j < (arr.length - i - 1); j++){
+      if (arr[j] > arr[j+1]){
+        const lesser = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
