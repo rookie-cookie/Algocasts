62fc2d6ec4755979da5d353ab022a9a3f21fe0f1
Jona Reyes <jonamreyes@gmail.com>
2018-07-05 20:03:26 -0400

    selection sort

--- sorting/index.js
+++ sorting/index.js
@@ -19,7 +19,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+//assume that the first number in the array is the minimum
+//then find if there is any number that has smaller value then swaps
+  for (let i = 0; i < arr.length; i++){
+    let indexOfMin = i;
+    for (let j = i+1; j < arr.length; j++){
+      if (arr[j] < arr[indexOfMin]){
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i){
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
